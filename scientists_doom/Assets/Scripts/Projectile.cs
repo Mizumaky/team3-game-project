30147b144d5fda4ad6eb3d738dca3f2ef82f0f34
Petr Ježek <jezekpe6@fel.cvut.cz>
2018-10-18 23:22:30 +0200

    further tweaks, getting master ready for fix

---
+++ scientists_doom/Assets/Scripts/Projectile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	public float lifetime = 5f;
+
+	void Start () {
+		StartCoroutine(DestroyCountdown());
+	}
+	
+	IEnumerator DestroyCountdown() {
+		yield return new WaitForSeconds(lifetime);
+		Destroy(gameObject);
+	}
+}
