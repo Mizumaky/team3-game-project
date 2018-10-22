dface9c7a03f08689a2429ca8ab00bf2faa1ec72
Vojtěch Vavera <vavervoj@fel.cvut.cz>
2018-10-22 14:35:37 +0200

    Barbarian attack change

---
+++ scientists_doom/Assets/Scripts/Player/PlayerAttacksBarbarian.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerAttacksBarbarian : MonoBehaviour {
+
+    public float distance = 50f;
+    public float projectileVelocity = 20f;
+
+    public GameObject projectilePrefab;
+    public Transform spawnPosition;
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Fire();
+        }
+	}
+
+    void Fire() {
+        animator.SetTrigger("attackTrigger");
+    }
+}
