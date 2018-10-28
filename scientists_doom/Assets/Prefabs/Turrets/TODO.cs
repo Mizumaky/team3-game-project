e154de4b82a0afa1cdd0558bf8e1b9d26266373b
Mizumaky <mullemi5@fel.cvut.cz>
2018-10-28 09:59:25 +0100

    delete all in my branch

--- scientists_doom/Assets/Prefabs/Turrets/TODO.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public class TODO : MonoBehaviour {
-
-	// - Make sound hit effects not to be bound to the particles AudioSource, get rid of the sound warning
-	// - bullet animation - do i really have to make scripting for that? think it could be done just by adding 
-    //animator dircetly to that thing, and just leave the controller Entry > Animation
-}
