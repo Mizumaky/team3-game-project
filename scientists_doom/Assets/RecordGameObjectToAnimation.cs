d0ce62b6febd667f737b894a4091f56ec063f412
Vojtěch Vavera <vavervoj@fel.cvut.cz>
2018-11-06 20:56:01 +0100

    GamePlayVideo scene Prepare virtual cameras

---
+++ scientists_doom/Assets/RecordGameObjectToAnimation.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor.Experimental.Animations;
+using UnityEngine;
+
+public class RecordGameObjectToAnimation : MonoBehaviour {
+
+    public AnimationClip clip;
+
+    private GameObjectRecorder gameObjectRecorder;
+
+	void Start () {
+        // Create recorder and record the script GameObject.
+        gameObjectRecorder = new GameObjectRecorder(gameObject);
+
+        // Bind all the Transforms on the GameObject and all its children.
+        gameObjectRecorder.BindComponentsOfType<Transform>(gameObject, true);
+    }
+
+    void LateUpdate()
+    {
+        if (clip == null)
+            return;
+
+        // Take a snapshot and record all the bindings values for this frame.
+        gameObjectRecorder.TakeSnapshot(Time.deltaTime);
+    }
+
+    void OnDisable()
+    {
+        if (clip == null)
+            return;
+
+        if (gameObjectRecorder.isRecording)
+        {
+            // Save the recorded session to the clip.
+            gameObjectRecorder.SaveToClip(clip);
+        }
+    }
+}
