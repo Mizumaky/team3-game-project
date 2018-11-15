1ec41875c6ab161f824d3ec56c91266d83769368
František Čmuchař <cmuchfra@fel.cvut.cz>
2018-11-15 21:35:59 +0100

    no message

--- scientists_doom/Assets/Scripts/Enemy/EnemyControls.cs
+++ scientists_doom/Assets/Scripts/Enemy/EnemyControls.cs
@@ -19,9 +19,11 @@ public class EnemyControls : MonoBehaviour {
 
   void Awake () {
     Init ();
+  }
 
+  void Start(){
     target = castle;
-    SetPathToTarget (target);
+    activeFollowCoroutine = StartCoroutine (AttackCastle());
 
     speed = 0;
   }
@@ -47,14 +49,15 @@ public class EnemyControls : MonoBehaviour {
   public void Aggro (Transform targetTransform) {
     Debug.Log ("Aggroing to " + targetTransform.name);
     target = targetTransform;
-    if (activeFollowCoroutine == null) {
-      Debug.Log ("Starting routine");
-      activeFollowCoroutine = StartCoroutine (CheckForTargetAndFollow ());
+    if(activeFollowCoroutine != null){
+      StopCoroutine(activeFollowCoroutine);
     }
+    Debug.Log ("Starting AttackPlayer coroutine");
+    activeFollowCoroutine = StartCoroutine (AttackPlayer ());
+    
   }
 
-  IEnumerator CheckForTargetAndFollow () {
-
+  private IEnumerator AttackPlayer () {
     if (enemyStats.isAlive ()) {
       float distance = Vector3.Distance (target.position, transform.position);
       while (target != null && target.gameObject.activeSelf && distance < distanceToFollowPlayer) {
@@ -72,7 +75,25 @@ public class EnemyControls : MonoBehaviour {
 
       // Reset to castle
       target = castle;
-      SetPathToTarget (target);
+      activeFollowCoroutine = StartCoroutine(AttackCastle());
+    }
+  }
+
+  private IEnumerator AttackCastle(){
+    if (enemyStats.isAlive ()) {
+      float distance = Vector3.Distance (target.position, transform.position);
+      while (target != null && target.gameObject.activeSelf) {
+        distance = Vector3.Distance (target.position, transform.position);
+        // If target close enough, attack and face it
+        if (distance <= 4) {
+          transform.rotation = CountLookRotation ();
+          animator.SetTrigger ("attackTrigger");
+        } else {
+          SetPathToTarget (target);
+          //Debug.Log ("Setting path");
+        }
+        yield return new WaitForSeconds (0.1f);
+      }
     }
   }
 
@@ -113,6 +134,7 @@ public class EnemyControls : MonoBehaviour {
 
   public void DisableCollision () {
     GetComponent<CapsuleCollider> ().enabled = false;
+    GetComponent<Rigidbody>().isKinematic = true;
   }
 
 }
