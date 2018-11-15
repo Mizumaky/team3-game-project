22d4741cacffca290033881f21760beca0e58c14
Vojta Vavera <vavervoj@fel.cvut.cz>
2018-11-15 21:20:08 +0100

    Castle takes damage, castle HP bar

--- scientists_doom/Assets/Scripts/UI/HUDScript.cs
+++ scientists_doom/Assets/Scripts/UI/HUDScript.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class HUDScript : MonoBehaviour {
-
+  [SerializeField] private Stats castleStats;
+  [SerializeField] private Image castleHealthSlider;
   [SerializeField] private Image healthSlider;
   [SerializeField] private Text healthText;
   [SerializeField] private Image xpSlider;
@@ -15,6 +16,13 @@ public class HUDScript : MonoBehaviour {
 
   // TODO: This can be made much faster (e.g. listener on character stats)
   private void LateUpdate () {
+    if(castleStats){
+      float maxCastleHealth = castleStats.GetTotalMaxHealth();
+      float castleHealth = castleStats.GetCurrentHealth();
+
+      castleHealthSlider.fillAmount = castleHealth / maxCastleHealth;
+    }
+
     if (CharacterManager.activeCharacterObject != null) {
       //get new playerstats component reference only on character change
       if (lastActiveCharObject != CharacterManager.activeCharacterObject) {
@@ -38,6 +46,7 @@ public class HUDScript : MonoBehaviour {
     } else {
       lastActiveCharObject = null;
     }
+
     woodText.text = ResourcesManager.wood.ToString();
     stoneText.text = ResourcesManager.stone.ToString();
     soulsText.text = ResourcesManager.souls.ToString();
