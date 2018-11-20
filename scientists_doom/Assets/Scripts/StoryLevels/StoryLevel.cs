99c2206dcdd0e2de445b39e8ed616301bc557ca0
Vojta Vavera <vavervoj@fel.cvut.cz>
2018-11-20 22:04:13 +0100

    Level scriptable object

---
+++ scientists_doom/Assets/Scripts/StoryLevels/StoryLevel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "StoryLevel", menuName = "StoryLevel", order = 2)]
+public class StoryLevel : ScriptableObject {
+	private int enemyCount;
+
+}
