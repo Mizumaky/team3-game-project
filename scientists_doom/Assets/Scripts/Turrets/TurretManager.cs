33b1dec7ffe67912057bc8e0355f2b59c76e575f
Petr Ježek <jezekpe6@fel.cvut.cz>
2018-11-23 22:30:07 +0100

    huntress model and prefab

--- scientists_doom/Assets/Scripts/Turrets/TurretManager.cs
+++ scientists_doom/Assets/Scripts/Turrets/TurretManager.cs
@@ -9,40 +9,41 @@ public class TurretManager : MonoBehaviour {
     //TODO change spawnpoint dle turretu, udelat nejaky dic nebo tak na zacatku
 
     public enum Turret { BarbarianTurret, WizardTurret, RangerTurret, WitchTurret }
-    [Header("Turrets and spawnpoints")]
+
+    [Header ("Turrets and spawnpoints")]
     public GameObject barbarianTurretPrefab;
     public GameObject wizardTurretPrefab;
-    public GameObject rangerTurretPrefab;
+    public GameObject huntressTurrentPrefab;
 
     public Transform SpawnPoint1;
     public Transform SpawnPoint2;
     public Transform SpawnPoint3;
 
     [Space]
-    [Header("In-game script references")]
+    [Header ("In-game script references")]
     public static GameObject activeTurretObject = null;
     //public static Turret activeTurret;
 
-    private void Start() {
+    private void Start () {
 
     }
 
-    private void Update() {
-        if (activeTurretObject == null && Input.GetKeyDown(KeyCode.T)) {
-            SpawnNewTurret(SpawnPoint1); //TODO multiplayer different spawnpoints
+    private void Update () {
+        if (activeTurretObject == null && Input.GetKeyDown (KeyCode.T)) {
+            SpawnNewTurret (SpawnPoint1); //TODO multiplayer different spawnpoints
         }
     }
 
-    private void SpawnNewTurret(Transform spawnPoint) {
+    private void SpawnNewTurret (Transform spawnPoint) {
         switch (CharacterManager.activeCharacter) {
             case CharacterManager.Character.Barbarian:
-                activeTurretObject = Instantiate(barbarianTurretPrefab, spawnPoint.position, spawnPoint.rotation, this.transform);
+                activeTurretObject = Instantiate (barbarianTurretPrefab, spawnPoint.position, spawnPoint.rotation, this.transform);
                 break;
             case CharacterManager.Character.Wizard:
-                activeTurretObject = Instantiate(wizardTurretPrefab, spawnPoint.position, spawnPoint.rotation, this.transform);
+                activeTurretObject = Instantiate (wizardTurretPrefab, spawnPoint.position, spawnPoint.rotation, this.transform);
                 break;
-            case CharacterManager.Character.Ranger:
-                activeTurretObject = Instantiate(rangerTurretPrefab, spawnPoint.position, spawnPoint.rotation, this.transform);
+            case CharacterManager.Character.Huntress:
+                activeTurretObject = Instantiate (huntressTurrentPrefab, spawnPoint.position, spawnPoint.rotation, this.transform);
                 break;
             default:
                 break;
