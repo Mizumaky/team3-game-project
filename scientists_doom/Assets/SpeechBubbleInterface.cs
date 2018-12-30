1ab48283113db03759a33002110dc4328500407a
Vojtěch Vavera <vavervoj@fel.cvut.cz>
2018-12-30 03:05:44 +0100

    SpeechBubble controller, interface <---working--->

---
+++ scientists_doom/Assets/SpeechBubbleInterface.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechBubbleInterface : MonoBehaviour
+{
+    public GameObject bubblePrefab;
+
+    private Vector3 spawnPosition;
+
+    void Start()
+    {
+        spawnPosition = Vector3.zero;
+        spawnPosition.y = GetComponent<Collider>().bounds.extents.y * 2;
+        SpawnSpeechBubble("The castle shall fall!!!", 10f);
+    }
+
+    public void SpawnSpeechBubble(string text, float duration){
+        GameObject bubble = Instantiate(bubblePrefab);
+        bubble.transform.SetParent(gameObject.transform, false);
+        bubble.transform.localPosition = spawnPosition;
+
+        bubble.GetComponent<SpeechBubbleController>().Show(text, duration);
+    }
+}
