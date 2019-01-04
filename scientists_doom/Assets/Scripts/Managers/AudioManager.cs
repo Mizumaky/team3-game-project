bc48df217bbcbceee6290d9ca8e3ff46a729f33a
Petr Ježek <jezekpe6@fel.cvut.cz>
2019-01-04 01:49:54 +0100

    music - night theme, theme blending, scene tweaks, FXAA

--- scientists_doom/Assets/Scripts/Managers/AudioManager.cs
+++ scientists_doom/Assets/Scripts/Managers/AudioManager.cs
@@ -4,5 +4,48 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+  public AudioSource dayAudioSource, nightAudioSource;
+  public float themeBlendSpeed;
+  private AudioSource activeSource;
 
+  private void Awake()
+  {
+    dayAudioSource.volume = 1;
+    nightAudioSource.volume = 0;
+    activeSource = dayAudioSource;
+    EventManager.StartListening("dayTheme", BlendDay);
+    EventManager.StartListening("nightTheme", BlendNight);
+  }
+
+  private void BlendDay()
+  {
+    StartCoroutine(BlendInDayTheme());
+  }
+
+  private void BlendNight()
+  {
+    StartCoroutine(BlendInNightTheme());
+  }
+
+  private IEnumerator BlendInDayTheme()
+  {
+    while (dayAudioSource.volume < 1)
+    {
+      dayAudioSource.volume += Time.deltaTime * themeBlendSpeed;
+      nightAudioSource.volume = 1 - dayAudioSource.volume;
+      yield return null;
+    }
+    activeSource = dayAudioSource;
+  }
+
+  private IEnumerator BlendInNightTheme()
+  {
+    Debug.Log("NIGHTTTTTT");
+    while (nightAudioSource.volume < 1)
+    {
+      nightAudioSource.volume += Time.deltaTime * themeBlendSpeed;
+      dayAudioSource.volume = 1 - nightAudioSource.volume;
+      yield return null;
+    }
+  }
 }
